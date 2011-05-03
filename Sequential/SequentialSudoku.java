6c6e1e87a967147537a8f28be55a7d5afe80467e
erkc <elgordito@gmail.com>
2011-05-03 15:03:26 -0400

    Sequential file/folder set up
    Change-Id: I934fe6294f6f23a6f4110f8805668405cdd36661

---
+++ Sequential/SequentialSudoku.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author emc6102
+ *
+ */
+public class SequentialSudoku {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
