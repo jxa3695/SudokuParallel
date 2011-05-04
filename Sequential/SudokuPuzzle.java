b2d2492f2d9ecab757c3df277a397abf3319e7c2
Jason Au <jxa3695@rit.edu>
2011-05-04 18:36:46 -0400

    Cell Class now has toString() method and uses the term "value" or "Value" in place of "answer" or "Answer". Add the row/column checker that set cell's value when it detects a singlely owned hint.

--- Sequential/SudokuPuzzle.java
+++ Sequential/SudokuPuzzle.java
@@ -58,4 +58,38 @@ public class SudokuPuzzle {
 		
 	}
 	
+	
+	
+	
+	/* Row and Column checker */
+	// collect any hints that is not shared by any other.
+	// if hint is not shared by any other, it is the answer/Value for its owner.
+	void RCChecker( Cell [] col ){
+		// index of these array represent the hint with the value of index+1
+		int [] hintCounter = new int[9]; // counter for hint
+		Cell [] owner = new Cell[9]; // first encountered ownere for hint
+
+		for (int cell=0; cell<9; cell++){ //iterate through the cells in col
+			if ( col[i].getAnswer()>0 ) next; // skip cells with answer set already
+
+			int [] hints = col[i].getHints(); // get the list of hints in 
+
+			for(int j =0; j<hints.length; j++){ 
+				hintVal = hints[j] - 1;
+				if( owner[hintVal] == 0 ) owner[hintVal] = cell ; // record a first seen hints owner
+				hintCounter[ hintVal ]++ ;  // for every hint seen increment its counter
+
+			}
+		}
+		
+		// go through hintCounter for any singlely owned hint(s)
+		for (int i=0; i<9; i++){
+			if (hintCounter[i] == 1) {
+				cell[i].setvalue( i );
+				//debugger
+				System.out.println(cell[i].toString())
+			}
+		}
+	}
+	
 }
