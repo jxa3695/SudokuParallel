d10f449c4a0d5c0eed68be020540ace23be7c2fd
erkc <elgordito@gmail.com>
2011-05-10 15:19:55 -0400

    Added calculations.txt only sequential column is correct. Put comments in CluSudoku for notes
    Change-Id: Iaa3b80e3143c3ae45697eb9d60bb74a353e720a5

--- Sequential/CluSudoku.java
+++ Sequential/CluSudoku.java
@@ -8,37 +8,44 @@ public class CluSudoku {
 	 * @param args
 	 */
 	static Comm world;
+
 	static int rank, size;
+
 	public static void main(String[] args) throws Exception {
 		// middle ware
 		Comm.init(args);
 		world = Comm.world();
 		size = world.size();
 		rank = world.rank();
-		
-		
+
 		if (args.length != 2) {
 			System.out.println("Usage: java SmpSudoku N infile");
 			System.exit(0);
 		}
-		
+
 		int n = Integer.parseInt(args[0]);
 		String filename = args[1];
-		
+
 		SudokuPuzzle puzzle = new SudokuPuzzle(filename, n);
-		System.out.println("Inital Puzzle");
-		puzzle.printPuzzle();
+		if (rank == 0) {
+			System.out.println("Inital Puzzle");
+			puzzle.printPuzzle();
+		}
 		long start = System.currentTimeMillis();
-		puzzle.solveClu( world, rank, size );
+		puzzle.solveClu(world, rank, size);
 		long stop = System.currentTimeMillis();
-		if (SudokuPuzzle.sharedCount.get() > 0) {
-			System.out.println("\nState of the puzzle when the algorithm failed");
-			System.out.println("count = " + SudokuPuzzle.sharedCount.get());
+		if (SudokuPuzzle.sharedCount.get() > 0 && rank == 0) {
+			System.out
+					.println("\nState of the puzzle when the algorithm failed");
+			System.out.println("count = "
+					+ SudokuPuzzle.sharedCount.get());
 		} else {
-			System.out.println("\nSolved Puzzle");
+			if( rank == 0 ) {
+				System.out.println("\nSolved Puzzle");
+			}
 		}
 		puzzle.printPuzzle();
-		
+
 		System.out.println((stop - start) + " msec");
 	}
 }
